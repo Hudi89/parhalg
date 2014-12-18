817f7957d2637d340398ada8c3baaf3d2468acb7
Németh, Bence <nemben@gmail.com>
2014-12-18 23:42:51 +0100

    Cleaned up ExampleServer and ExampleClient

--- src/inf/elte/parhalg/example/ExampleServer.java
+++ src/inf/elte/parhalg/example/ExampleServer.java
@@ -1,76 +1,80 @@
 package inf.elte.parhalg.example;
 
 import inf.elte.parhalg.connection.PacketProcessor;
-import inf.elte.parhalg.connection.ServerThread;
 import inf.elte.parhalg.connection.Responder;
+import inf.elte.parhalg.connection.ServerThread;
+import inf.elte.parhalg.packet.FilesendPacket;
+import inf.elte.parhalg.packet.MessagePacket;
+import inf.elte.parhalg.packet.Packet;
+import inf.elte.parhalg.packet.PacketType;
+import inf.elte.parhalg.serverstorage.FreeSpaceListener;
+import inf.elte.parhalg.serverstorage.USBWatcher;
 
 import java.io.IOException;
-
-import inf.elte.parhalg.packet.*;
 import java.nio.file.Files;
-import java.io.File;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import inf.elte.parhalg.serverstorage.USBWatcher;
-import inf.elte.parhalg.serverstorage.FreeSpaceListener;
+public class ExampleServer implements PacketProcessor, FreeSpaceListener {
 
-public class ExampleServer implements PacketProcessor,FreeSpaceListener {
-	private String mountPoint;
+	private static final Logger LOG = Logger.getAnonymousLogger();
 
-	public ExampleServer(String mountPoint, int port) throws IOException{
-		this.mountPoint = mountPoint; 
-		new USBWatcher(Paths.get(mountPoint), this);
+	private final Path mountPoint;
+
+	public ExampleServer(Path mountPoint, int port) throws IOException {
+		this.mountPoint = mountPoint;
+		new USBWatcher(mountPoint, this);
 		new ServerThread(this, port).start();
 	}
 
 	@Override
 	public void freeSpaceWarningCallback(long freeSpace) {
-		Responder.broadcast(new MessagePacket("Limit reached, available space:" + (freeSpace/1024) + " MB!"));
+		Responder.broadcast(new MessagePacket("Limit reached, available space:" + freeSpace + " MB!"));
 	}
-	
+
 	@Override
 	public void process(Responder responder, Packet packet) {
-		if(packet.getType() == PacketType.MESSAGE){
-			System.out.println(packet);
-		}else if(packet.getType() == PacketType.FILESEND){
-			FilesendPacket p = (FilesendPacket)packet;
-			try{
-				System.out.println(mountPoint+p.getPath());
-
-
-				String backupPath = mountPoint+p.getBackupName()+"/"+p.getPath();
-				File backupPathFile = new File(backupPath);
-				backupPathFile.getParentFile().mkdirs();
-
-				Files.write(backupPathFile.toPath(),p.getData());
-			}catch(IOException e){
-				System.out.println("File write ... up" + p.getPath() + "!");
-				e.printStackTrace();
+		if (packet.getType() == PacketType.FILESEND) {
+			FilesendPacket filesend = (FilesendPacket) packet;
+			try {
+				Path backupPath = mountPoint.resolve(filesend.getBackupName()).resolve(filesend.getPath());
+				if (!backupPath.startsWith(mountPoint)) {
+					LOG.log(Level.SEVERE, "Backup path not in mount point!", filesend);
+					return;
+				}
+				Files.createDirectories(backupPath.getParent());
+				Files.write(backupPath, filesend.getData());
+			} catch (IOException ex) {
+				LOG.log(Level.SEVERE, "Could not write file...", ex);
 			}
+		} else {
+			System.out.println(packet);
 		}
 	}
 
-
-	public static void main(String[] args) throws IOException {
-		if(args.length < 2){
-			help();
-			return;
-		}		
-		String mountPoint = args[0];
-		if(!mountPoint.endsWith("/")){
-			mountPoint += "/";
-		}
-		if(!(new File(mountPoint).exists())){
-			System.out.println("The mount point doesn't exists!");
-			System.out.println("");
-			return;
-		}
-		new ExampleServer(mountPoint,Integer.parseInt(args[1]));
-	}
-
-	private static void help(){
+	private static void printHelp() {
 		System.out.println("Usage:");
 		System.out.println("ExampleServer [backup mount point] [port to listen]");
+	}
+
+	public static void main(String[] args) throws IOException {
+		if (args.length < 2) {
+			printHelp();
+			return;
+		}
+
+		String mountPoint = args[0];
+		int port = Integer.parseInt(args[1]);
+
+		Path mountPointPath = Paths.get(mountPoint);
+		if (!Files.exists(mountPointPath)) {
+			System.err.println("The mount point doesn't exists!");
+			return;
+		}
+
+		new ExampleServer(mountPointPath, port);
 	}
 
 }
