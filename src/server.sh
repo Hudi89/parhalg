207c13c00b465f0b98d73f4e8d5b96b302702032
Jackson Arboreus <jacarb@fail.net>
2014-12-22 16:27:00 +0100

    pretend that the code needs to be secure

--- src/server.sh
+++ src/server.sh
@@ -2,7 +2,7 @@ javac inf/elte/parhalg/example/ExampleServer.java
 if [ $? -ne 0 ]; then
 	echo "Compile Failed, script stopped!"
 else
-	java inf.elte.parhalg.example.ExampleServer $@
+	java inf.elte.parhalg.example.ExampleServer "$@"
 fi
 
 #Example:
